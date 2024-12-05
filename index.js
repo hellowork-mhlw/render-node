6fcff1184802d592fdf5a1d7acd94fb1c22180dc
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2024-12-05 23:41:33 +0000

    a

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 // index.js
 const express = require('express')
+const { exec } = require("child_process")
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send(`Hello2 ${process.env.HELLO || ''}`)
 })
+
+// /ping エンドポイント
+app.get("/ping", (req, res) => {
+  const target = req.query.target ?? '8.8.8.8'; // クエリパラメータで対象ホストを指定
+  if (!target) {
+      return res.status(400).send("Target is required. Use /ping?target=<hostname>");
+  }
+
+  // ping コマンドの実行
+  exec(`ping -c 4 ${target}`, (error, stdout, stderr) => {
+      if (error) {
+          console.error(`Error: ${error.message}`);
+          return res.status(500).send(`Error: ${error.message}`);
+      }
+      if (stderr) {
+          console.error(`Stderr: ${stderr}`);
+          return res.status(500).send(`Stderr: ${stderr}`);
+      }
+
+      // 成功時のレスポンス
+      res.send(`<pre>${stdout}</pre>`);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port:${PORT}`)
